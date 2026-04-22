ddaba7772acb80e8fb275a9c9806dea3ed8008e8
Samuel Chávez <chavez@MacBook-Air-de-Samuel.local>
2026-04-22 12:10:59 +0200

    feat: add argparse support for CLI options

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 from scraper import scrape_headlines
 from reporter import generate_numbered_list, get_top_titles_by_length, count_items, write_report
-
+import argparse
 def main():
-    output_file_path = input ("Name output file (default: report.txt): ")
-    if output_file_path == "":
-        output_file_path = "report.txt"
-    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--top" , type=int, default=2)
+    parser.add_argument("--output", type=str, default="report.txt")
+
+    args = parser.parse_args()
+
+    n = args.top
+    output_file_path = args.output
     if not output_file_path.endswith(".txt"):
         output_file_path += ".txt"
-    
-    user_input = input("How many top titles? (default: 2)")
-    if user_input == "":
-        n = 2
-    else:
-        try:
-            n = int(user_input)
-        except ValueError:
-            print("Invalid input, using default value (2).")
-            n = 2
+
     if n <= 0:
         print("Invalid input, using default value (2).")
         n = 2
@@ -29,7 +24,7 @@ def main():
     top_titles_by_length = get_top_titles_by_length(data, n)
     total_items = count_items(data)
     write_report(output_file_path, numbered_list, top_titles_by_length, total_items)
-    print("Report generated:" , output_file_path)
+    print("Report generated:", output_file_path)
 
 
 if __name__ == "__main__":
